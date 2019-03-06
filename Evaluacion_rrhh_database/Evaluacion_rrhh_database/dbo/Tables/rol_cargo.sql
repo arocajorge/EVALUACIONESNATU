8516a0653b958398c7864d15e4663df811af2df6
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-03-06 12:47:04 -0500

    .

--- Evaluacion_rrhh_database/Evaluacion_rrhh_database/dbo/Tables/rol_cargo.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[rol_cargo] (
-    [IdCargo]        INT           NOT NULL,
-    [rc_codigo]      VARCHAR (50)  NULL,
-    [rc_descripcion] VARCHAR (300) NOT NULL,
-    [estado]         BIT           NOT NULL,
-    CONSTRAINT [PK_rol_cargo_1] PRIMARY KEY CLUSTERED ([IdCargo] ASC)
-);
-
--- Evaluacion_rrhh_database/Evaluacion_rrhh_database/dbo/Tables/rol_empleado_x_correo_x_periodo.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[rol_empleado_x_correo_x_periodo] (
-    [IdPeriodo]      INT           NOT NULL,
-    [IdEmpleado]     NUMERIC (18)  NOT NULL,
-    [Secuencia]      INT           NOT NULL,
-    [Correo_enviado] BIT           NULL,
-    [Observacion]    VARCHAR (500) NULL,
-    CONSTRAINT [PK_rol_empleado_x_correo_x_periodo] PRIMARY KEY CLUSTERED ([IdPeriodo] ASC, [IdEmpleado] ASC, [Secuencia] ASC)
-);
-
